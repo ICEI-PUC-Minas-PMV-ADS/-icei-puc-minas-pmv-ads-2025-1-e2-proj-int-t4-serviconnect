afde9ea1accabb98c99ecaf3a0a01eb9752cad9d
Fernando Cardoso Nascimento <fernando.cardosonasc@gmail.com>
2025-05-15 22:58:19 -0300

    Adicionar arquivos de projeto.

---
+++ Serviconnect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Serviconnect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
